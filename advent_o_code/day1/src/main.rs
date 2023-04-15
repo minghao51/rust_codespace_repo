a7a6e59b5ce25c3c889afa2e37b1b8fb7c67633c
minghao <howt51@gmail.com>
2023-04-15 13:01:08 +0000

    main with load

--- advent_o_code/day1/src/main.rs
+++ advent_o_code/day1/src/main.rs
@@ -1,22 +1,11 @@
-use itertools::Itertools;
-fn main() -> color_eyre::Result<()> {
+
+pub mod part1;
+pub mod part2;
+
+fn main(){
     // https://fasterthanli.me/series/advent-of-code-2022/part-1
     // https://www.ericburden.work/blog/2022/12/01/advent-of-code-2022-day-01/
 
-    color_eyre::install()?;
-
-    let max = std::fs::read_to_string("data.txt").unwrap() //# include_str!("")
-        .lines()
-        .map(|v| v.parse::<u64>().ok())
-        .batching(|it| {
-            let mut sum = None;
-            while let Some(Some(v)) = it.next() {
-                sum = Some(sum.unwrap_or(0) + v);
-            }
-            sum
-        })
-        .max();
-    println!("{max:?}");
-
-    Ok(())
+    part1::solve1().ok();
+    part2::solve2().ok();
 }
---
+++ advent_o_code/day1/src/part1.rs
@@ -0,0 +1,22 @@
+use itertools::Itertools;
+pub fn solve1() -> color_eyre::Result<()> {
+    // https://fasterthanli.me/series/advent-of-code-2022/part-1
+    // https://www.ericburden.work/blog/2022/12/01/advent-of-code-2022-day-01/
+
+    color_eyre::install()?;
+
+    let max = std::fs::read_to_string("data.txt").unwrap() //# include_str!("")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .max();
+    println!("{max:?}");
+
+    Ok(())
+}
