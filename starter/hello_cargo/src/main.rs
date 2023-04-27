fa93661b53cb8875a69a6548f9f63cbbbfde9157
minghao <howt51@gmail.com>
2023-04-27 16:57:12 +0000

    rust explore day 3

--- starter/hello_cargo/src/main.rs
+++ starter/hello_cargo/src/main.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let mut count:HashMap<char,i32> = HashMap::new();
+    count.insert('s',2);
+    let val = count.get(&'s').unwrap(); //mut
+
+    println!("test: {val}");
+    let val = count.get(&'s').unwrap()+1; //mut
+    println!("test: {val}");
+
+    // # default dict
+    // https://stackoverflow.com/questions/41417660/how-does-one-create-a-hashmap-with-a-default-value-in-rust
+    let val = count.get(&'a').cloned().unwrap_or(0);
+    println!("test: {val}");
+    // println!("You guessed: {guess}");
+
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
 }
